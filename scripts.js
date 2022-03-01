8225e7eb60cff772d1fcd2781952c23d26339d60
Your Name~ <timrogersj@gmail.com>
2022-02-28 20:12:16 -0600

    completed 23.8.1 and 23.8.2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    let flightStatus = document.getElementById("flightStatus");
+    let bgflightScreen = document.getElementById("shuttleBackground");
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    let takeoffButton = document.getElementById("takeoff");
+    takeoffButton.addEventListener("click", function() {
+        let shuttleReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (shuttleReady) {
+            flightStatus.innerText = "Shuttle in flight.";
+            bgflightScreen.style.backgroundColor = "blue";
+            shuttleHeight.innerText = "10000";
+        }
+    })
+})
